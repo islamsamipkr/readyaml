e2f5a776b5d0612316643d52676182fab60fe808
Sami Islam <islam.samipkr@gmail.com>
2023-10-03 20:55:32 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
-linuxapp:
-  - name: firstlinuxapp
-    resource_group: azureresourcegroup
+locals{
+  linux_app=[for f in fileset("${path.module}/configs", "[^_]*.yaml") : yamldecode(file("${path.module}/configs/${f}"))]
+  linux_app_list = flatten([
+    for app in local.linux_app : [
+      for linuxapps in try(app., []) :
+}
